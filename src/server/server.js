9e9f77e0b587d23e64d62863c61332fb4491da1b
WIVSW <olegspostnikov@gmail.com>
2018-05-13 16:21:59 +0500

    Install express and create server

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,14 @@
+const express = require('express');
+const http = require('http');
+
 const Connection = require('./connection');
 
 const connection = new Connection('localhost', '27017', 'DashboardSPA');
 
 connection.init();
+
+let app = express();
+
+http
+    .createServer(app)
+    .listen(3000);

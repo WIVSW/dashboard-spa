c68be39130131bd3babe15644a47e9cb26430756
WIVSW <olegspostnikov@gmail.com>
2018-05-26 18:47:58 +0500

    Add css compilation and autoprefixer

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
-
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 
 module.exports = {
@@ -13,7 +15,36 @@ module.exports = {
 			{
 				use: 'babel-loader',
 				test: /\.(js|jsx)$/
+			},
+			{
+				test: /\.scss$/,
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						{ loader: 'css-loader' },
+						{
+							loader: 'postcss-loader',
+							options: {
+								plugins: [
+									autoprefixer({
+										browsers:['ie >= 8', 'last 4 version']
+									}),
+									cssnano({
+										discardComments: {
+											removeAll: true
+										}
+									})
+
+								]
+							}
+						},
+						{ loader: 'sass-loader' }
+					]
+				})
 			}
 		]
-	}
+	},
+	plugins: [
+		new ExtractTextPlugin("index.[chunkhash].css"),
+	]
 };

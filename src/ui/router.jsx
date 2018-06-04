d899e401f078c1cbd8a536127fa41190ab326285
WIVSW <olegspostnikov@gmail.com>
2018-06-05 00:28:22 +0500

    Finish Router

--- src/ui/router.jsx
+++ src/ui/router.jsx
@@ -2,15 +2,27 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Home from './pages/home/home';
+import Products from './pages/products/products';
+import Table from './pages/table/table';
+import Tables from './pages/tables/tables';
 import Login from './pages/login/login';
+import Menu from './pages/menu/menu';
+import Menus from './pages/menus/menus';
+import NotFound from './pages/not-found/not-found';
 
 
 
 export default () => (
 	<BrowserRouter>
 		<Switch>
-			<Route path="/login" component={Login}/>
+			<Route path="/products/" component={Products}/>
+			<Route path="/tables/:id" component={Table}/>
+			<Route path="/tables/" component={Tables}/>
+			<Route path="/login/" component={Login}/>
+			<Route path="/menus/:id" component={Menu}/>
+			<Route path="/menus/" component={Menus}/>
 			<Route path="/" exact component={Home}/>
+			<Route component={NotFound}/>
 		</Switch>
 	</BrowserRouter>
 );

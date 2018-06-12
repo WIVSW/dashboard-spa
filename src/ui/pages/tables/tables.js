f42ed24998ed2de290b9eda4de36f27eb9e3b4e4
WIVSW <olegspostnikov@gmail.com>
2018-06-13 00:16:00 +0500

    Add table component

--- src/ui/pages/tables/tables.js
+++ src/ui/pages/tables/tables.js
@@ -1,13 +1,31 @@
 import React, { PureComponent } from 'react';
+import Table from '../../blocks/table/table.jsx';
 
 
 
 export default class extends PureComponent {
 	render() {
+		const table = {
+			head: [
+				'Название',
+				'Описание',
+				'Цена'
+			],
+			body: [
+				{
+					Название: 'Cырный раф',
+					Описание: 'Очень вкусный напиток',
+					Цена: 123
+				},
+				{
+					Название: 'Горячий шоколад',
+					Описание: 'Очень вкусный напиток',
+					Цена: 666.06
+				}
+			]
+		};
 		return (
-			<div>
-				Tables
-			</div>
+			<Table table={table}/>
 		);
 	}
 }

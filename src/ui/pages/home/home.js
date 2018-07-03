7b0f28f376c84bb03c889739b261ec0a278012ac
WIVSW <olegspostnikov@gmail.com>
2018-07-04 00:10:02 +0500

    Add content for 404 page

--- src/ui/pages/home/home.js
+++ src/ui/pages/home/home.js
@@ -4,8 +4,17 @@ import Page from '../base';
 
 
 class Home extends Page {
+	preload() {
+		return Promise.resolve({ data: {} })
+	}
+
 	getTemplate() {
-		return (<div>Home</div>)
+		return (
+			<div style={{padding: '30px'}}>
+				<p>Someday this page will contain some very important information but not today.</p>
+				<p>So please check the other pages.</p>
+			</div>
+		)
 	}
 }
 
--- src/ui/pages/not-found/not-found.js
+++ src/ui/pages/not-found/not-found.js
@@ -5,9 +5,15 @@ import React, { PureComponent } from 'react';
 export default class extends PureComponent {
 	render() {
 		return (
-			<div>
-				404
-			</div>
+			<img
+				src="https://i.pinimg.com/originals/59/2e/5c/592e5c32cbecb392f444b79950cb9e7e.jpg"
+				alt="Nothing to see here"
+
+				style={{
+					display: 'block',
+					margin: '0 auto'
+				}}
+			/>
 		);
 	}
 }

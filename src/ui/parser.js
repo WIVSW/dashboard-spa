50ab6183aa5d5c289f6fcdfea86141328ebbb5f5
WIVSW <olegspostnikov@gmail.com>
2018-07-05 01:40:34 +0500

    Add reading table

--- src/ui/parser.js
+++ src/ui/parser.js
@@ -1,9 +1,38 @@
+import XLSX from 'xlsx';
+
+
+
 class Parser {
-	constructor() {}
+	constructor() {
+		this._rABS = true;
+	}
 
 	parse(table) {
 		console.log(table);
+
+		return this
+			._readTable(table)
+			.then((workbook) => {
+				console.log(workbook);
+			});
 	}
+
+	_readTable(table) {
+		return new Promise((resolve, reject) => {
+			const reader = new FileReader();
+
+			reader.addEventListener('load', (e) => {
+				let data = e.target.result;
+				if (!this._rABS) data = new Uint8Array(data);
+				const workbook = XLSX.read(data, {type: this._rABS ? 'binary' : 'array'});
+
+				resolve(workbook);
+			});
+
+			this._rABS ? reader.readAsBinaryString(table) : reader.readAsArrayBuffer(table);
+		});
+	}
+
 }
 
 export default Parser;
